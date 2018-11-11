4542e70f9c5d8196bb3feaf2cef799163ff477c1
DESKTOP-9S722SE\mistre83 <mistre83@gmail.com>
2018-11-11 22:26:24 +0100

    fix

--- lib/src/blocs/validators.dart
+++ lib/src/blocs/validators.dart
@@ -1,12 +1,12 @@
 import "dart:async";
 
 class Validators {
-  final valitateEmail = StreamTransformer<String, String>.fromHandlers(
+  final validateEmail = StreamTransformer<String, String>.fromHandlers(
     handleData: (email, sink) {
       if (email.contains('@')) {
         sink.add(email);
       } else {
-        sink.addError('Enter a valid email');5
+        sink.addError('Enter a valid email');
       }
     }
   );
